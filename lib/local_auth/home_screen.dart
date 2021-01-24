1777cca8f4e151187f6190686306047ce971a256
Jeel Bhatti <jeelbhatti9@gmail.com>
2021-01-24 21:18:55 +0530

    local authentication

--- lib/local_auth/home_screen.dart
+++ lib/local_auth/home_screen.dart
@@ -3,110 +3,17 @@ import 'welcome_screen.dart';
 import 'dart:async';
 import 'package:local_auth/local_auth.dart';
 
-import 'package:flutter/material.dart';
-import 'package:local_auth/local_auth.dart';
-
-class HomeScreenLocalAuth extends StatefulWidget {
-  HomeScreenLocalAuth({Key key}) : super(key: key);
-
+class HomeScreenLocalAuth extends StatelessWidget {
   @override
-  _HomeScreenLocalAuthState createState() => _HomeScreenLocalAuthState();
-}
-
-class _HomeScreenLocalAuthState extends State<HomeScreenLocalAuth> {
-  final LocalAuthentication auth=LocalAuthentication();
-  bool _canCheckBiometrics;
-  List<BiometricType> _availableBiometrics;
-  String _authorized = 'Not Authorized';
-  bool _isAuthenticating = false;
-
-  Future<void> _checkBiometrics() async {
-    bool canCheckBiometrics;
-    try {
-      canCheckBiometrics = await auth.canCheckBiometrics;
-    } on Exception catch (e) {
-      print(e);
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _canCheckBiometrics = canCheckBiometrics;
-    });
-  }
-
-  Future<void> _getAvailableBiometrics() async {
-    List<BiometricType> availableBiometrics;
-    try {
-      availableBiometrics = await auth.getAvailableBiometrics();
-    } on Exception catch (e) {
-      print(e);
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _availableBiometrics = availableBiometrics;
-    });
-  }
-
-  Future<void> _authenticate() async {
-    bool authenticated = false;
-    try {
-      setState(() {
-        _isAuthenticating = true;
-        _authorized = 'Authenticating';
-      });
-      authenticated = await auth.authenticateWithBiometrics(
-          localizedReason: 'Scan your fingerprint to authenticate',
-          useErrorDialogs: true,
-          stickyAuth: true);
-      setState(() {
-        _isAuthenticating = false;
-        _authorized = 'Authenticating';
-      });
-    } on Exception catch (e) {
-      print(e);
-    }
-    if (!mounted) return;
-
-    final String message = authenticated ? 'Authorized' : 'Not Authorized';
-    setState(() {
-      _authorized = message;
-    });
-  }
-
-  void _cancelAuthentication() {
-    auth.stopAuthentication();
-  }
-
-   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Plugin example app'),
+        title: Text("Authentication"),
+        centerTitle: true,
       ),
-      body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text('Can check biometrics: $_canCheckBiometrics\n'),
-                RaisedButton(
-                  child: const Text('Check biometrics'),
-                  onPressed: _checkBiometrics,
-                ),
-                Text('Available biometrics: $_availableBiometrics\n'),
-                RaisedButton(
-                  child: const Text('Get available biometrics'),
-                  onPressed: _getAvailableBiometrics,
-                ),
-                Text('Current State: $_authorized\n'),
-                RaisedButton(
-                  child: Text(_isAuthenticating ? 'Cancel' : 'Authenticate'),
-                  onPressed:
-                      _isAuthenticating ? _cancelAuthentication : _authenticate,
-                )
-              ])),
-    ));
+      body: Container(
+        child: Center(child: RaisedButton(onPressed:(){},child:Text("Finger-print",style: TextStyle(color: Colors.black87),),),),
+      ),
+    );
   }
 }
